7ba74de15c85133cb8b6626e8082e937dec9d925
Nikola Risek <nikolarisek001@gmail.com>
2022-07-01 22:11:25 +0200

    Changed text for push counter

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Push counter:',
+              'I am counting:',
             ),
             Text(
               '$_counter',
